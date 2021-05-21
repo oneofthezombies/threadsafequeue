7a2502e1b32634ea2112cc40f296aef7a01819d2
oneofthezombies <hunhoekim@gmail.com>
2021-05-22 02:09:14 +0900

    modified wait_for condition.

--- ThreadSafeQueue.hpp
+++ ThreadSafeQueue.hpp
@@ -6,28 +6,49 @@
 #include <mutex>
 
 template<typename T>
-class ThreadSafeQueue {
-public:
+struct ThreadSafeQueue {
     ThreadSafeQueue(std::atomic_bool& isDone)
         : isDone_(isDone) {}
+
+    ~ThreadSafeQueue() {
+        signal_.notify_all();
+    }
 
     void push(T&& data) {
         {
             std::unique_lock<std::mutex> lock(mutex_);
             queue_.push(std::move(data));
         }
-        signal_.notify_one();
+        signal_.notify_all();
     }
 
     bool tryPop(std::chrono::milliseconds&& timeout, T& outData) {
         std::unique_lock<std::mutex> lock(mutex_);
-        if (!signal_.wait_for(lock, std::move(timeout), [this](){ return isDone_ || !queue_.empty(); }) || isDone_) {
+        if (!signal_.wait_for(lock, std::move(timeout), [this](){ return isDone_ || !queue_.empty(); })) {
+            return false;
+        }
+
+        if (isDone_) {
+            return false;
+        }
+
+        if (queue_.empty()) {
             return false;
         }
 
         outData = std::move(queue_.front());
         queue_.pop();
         return true;
+    }
+
+    size_t size() const {
+        std::unique_lock<std::mutex> lock(mutex_);
+        return queue_.size();
+    }
+
+    bool empty() const {
+        std::unique_lock<std::mutex> lock(mutex_);
+        return queue_.empty();
     }
 
     ThreadSafeQueue(ThreadSafeQueue&&) = delete;
@@ -39,3 +60,4 @@ private:
     std::queue<T> queue_;
     std::atomic_bool& isDone_;
 };
+
